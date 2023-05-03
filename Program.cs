afab5857e62be28e446df0cb1c849aec825ee1d3
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-05-03 20:06:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_2_ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p1;
+            double p2;
+            double n;
+            Console.Write("Digite o valor de p1: ");
+            p1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de p2: ");
+            p2 = double.Parse(Console.ReadLine());
+            n = (p1 + (2 * p2)) / 3;
+            if (n >= 5)
+                Console.Write("aluno aprovado: ");
+            else
+                Console.Write("aluno reprovado: ");
+
+
+        }
+    }
+}
